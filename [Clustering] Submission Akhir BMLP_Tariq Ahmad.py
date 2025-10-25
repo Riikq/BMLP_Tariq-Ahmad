248afb1c996421e0c229b7b78ac5e53bbec9597b
Riikq <tariqahmad0510@gmail.com>
2025-10-25 14:03:09 +0700

    mk files

--- [Clustering] Submission Akhir BMLP_Tariq Ahmad.py
+++ [Clustering] Submission Akhir BMLP_Tariq Ahmad.py
@@ -27,12 +27,32 @@ df.info()
 # Last checking gunakan columns.tolist() untuk checking seluruh fitur yang ada.
 # Perbaiki kode di bawah ini tanpa menambahkan atau mengurangi cell code ini.
 # ___.columns.tolist()
-#
+
 # Menangani data yang hilang (bisa menggunakan dropna() atau metode imputasi fillna()).
 # Menghapus data duplikat menggunakan drop_duplicates().
-#
+
 # Melakukan Handling Outlier Data berdasarkan jumlah outlier, apakah menggunakan metode drop atau mengisi nilai tersebut.
 # Melakukan binning data berdasarkan kondisi rentang nilai pada fitur numerik,
 # lakukan pada satu sampai dua fitur numerik.
 # Silahkan lakukan encode hasil binning tersebut menggunakan LabelEncoder.
 # Pastikan kamu mengerjakan tahapan ini pada satu cell.
+
+# menyimpan model menggunakan joblib
+# import joblib
+# joblib.dump(___, "model_clustering.h5")
+
+# Menghitung dan menampilkan nilai Silhouette Score.
+# Membuat visualisasi hasil clustering
+
+# Membangun model menggunakan PCA.
+# ___ = PCA (n_components=<x>)
+# ___ = ___.fit_transform(___)
+# Menyimpan data PCA sebagai Dataframe dengan nama PCA_<numbers>
+# <data_final> = pd.DataFrame(___, columns=['PCA1', 'PCA2', <sesuaikan dengan jumlah n>])
+# Pastikan kamu membangun model Kmeans baru dengan data yang sudah dimodifikasi melalui PCA.
+# ___ = KMeans(n_clusters=<x>)
+# ___.fit(<data_final>)
+
+# Simpan model PCA sebagai perbandingan dengan menjalankan cell code ini joblib.dump(model, "PC_model_clustering.h5")
+# Pastikan yang disiimpan model yang sudah melalui .fit berdasarkan datasetyang sudah dilakukan PCA
+# joblib.dump(___, "PCA_model_clustering.h5")
