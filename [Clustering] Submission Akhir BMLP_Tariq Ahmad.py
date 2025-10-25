81900b4ecb436c1bd07b7510b0421b377da919aa
Riikq <tariqahmad0510@gmail.com>
2025-10-25 12:18:25 +0700

    tasks

--- [Clustering] Submission Akhir BMLP_Tariq Ahmad.py
+++ [Clustering] Submission Akhir BMLP_Tariq Ahmad.py
@@ -16,3 +16,23 @@ df.head()
 df.info()
 
 # Menampilkan statistik deskriptif dataset dengan menjalankan describe
+
+# Mengecek dataset menggunakan isnull().sum()
+# Mengecek dataset menggunakan duplicated().sum()
+# Melakukan feature scaling menggunakan MinMaxScaler() atau StandardScalar() untuk fitur numerik.
+# Pastikan kamu menggunakan function head setelah melakukan scaling.
+# Melakukan drop pada kolom yang memiliki keterangan id dan IP Address
+# Melakukan feature encoding mengguanakan LabelEncoder() untuk fitur kategorikal.
+# Pastikan kamu menggunakan function head setelah melakukan encoding.
+# Last checking gunakan columns.tolist() untuk checking seluruh fitur yang ada.
+# Perbaiki kode di bawah ini tanpa menambahkan atau mengurangi cell code ini.
+# ___.columns.tolist()
+#
+# Menangani data yang hilang (bisa menggunakan dropna() atau metode imputasi fillna()).
+# Menghapus data duplikat menggunakan drop_duplicates().
+#
+# Melakukan Handling Outlier Data berdasarkan jumlah outlier, apakah menggunakan metode drop atau mengisi nilai tersebut.
+# Melakukan binning data berdasarkan kondisi rentang nilai pada fitur numerik,
+# lakukan pada satu sampai dua fitur numerik.
+# Silahkan lakukan encode hasil binning tersebut menggunakan LabelEncoder.
+# Pastikan kamu mengerjakan tahapan ini pada satu cell.
