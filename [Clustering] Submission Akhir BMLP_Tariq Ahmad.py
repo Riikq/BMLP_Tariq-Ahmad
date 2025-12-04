7cee6b3af53174640de175953251fc010f769c4e
Riikq <tariqahmad0510@gmail.com>
2025-12-05 06:04:23 +0700

    rm libs

--- [Clustering] Submission Akhir BMLP_Tariq Ahmad.py
+++ [Clustering] Submission Akhir BMLP_Tariq Ahmad.py
@@ -1,19 +1,19 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+# import pandas as pd
+# import matplotlib.pyplot as plt
 from yellowbrick.cluster import KElbowVisualizer
 
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.metrics import sillhouette_score
 
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTbg5WVW6W3c8SPNUGc3A3AL-AG32TPEQGpdzARfNICMsLFI0LQj0jporhsLCeVhkN5AoRsTkn08AYl/pub?output=csv"
-df = pd.read_csv(url)
+# df = pd.read_csv(url)
 
 # Load data
 # Tampilkan 5 baris pertamaa dengan fungsi head.
-print(df.head())
+# print(df.head())
 
 # Tinjau jumlah baris kolom dan jenis data dalam dataset dengan info.
-df.info()
+# df.info()
 
 # Menampilkan statistik deskriptif dataset dengan menjalankan describe
 
